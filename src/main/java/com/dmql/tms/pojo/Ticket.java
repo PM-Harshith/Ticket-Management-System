7017084dcc4bbc2b1211c3274190d032f9eabb69
Harshith P M <harshithpm@harshiths-mbp-2.home>
2022-11-24 21:02:24 -0500

    Changes

--- src/main/java/com/dmql/tms/pojo/Ticket.java
+++ src/main/java/com/dmql/tms/pojo/Ticket.java
@@ -1,9 +1,9 @@
 package com.dmql.tms.pojo;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Ticket {
     private String id;
     private int ticketId;
